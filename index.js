e7947b8b3c9f77a0f8acabadd8cc78dbc26b710e
leebit025 <kizianmoore@gmail.com>
2025-03-03 18:00:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,92 @@
-let radius = 7;
-const pi = 3.14;
+//object literal
+let bob = {
+    fname: "bob",
+    lname: "smith",
+    age: 18,
+    height: 6,
+    transcript:[
+      {
+        course: 'INFO 1603',
+        grades: [ 89, 34, 67 ]
+      },
+      {
+        course: 'INFO 1601',
+        grades: [ 89, 34, 67 ]
+      }
+    ]
+  };
+  
+  let sally = {
+    fname: "sally",
+    lname: "smith",
+    age: 18,
+    height: 6,
+    transcript:[
+      {
+        course: 'INFO 1601',
+        grades: [ 100, 89, 79 ]
+      }
+    ]
+  };
+  
+  let paul = {
+    fname: "paul",
+    lname: "smith",
+    age: 18,
+    height: 6,
+    transcript:[
+      {
+        course: 'INFO 1600',
+        grades: [ 89, 34, 67 ]
+      }
+    ]
+  };
+  
+  
+  const students = [bob, sally, paul];
 
-let area = radius * radius * pi;
-console.log(area)
+  function getAverageGrade(student,course){
+    for(let record of student.transcript){
+        if(record.course === course){
+            let sum = 0;
+            for(let grade of record.grades){
+                sum += grade;
+            }
+            return sum / record.grades.length;
+        }
+    }
+    return -1;
+  }
+  
 
-let str1 ="Hello World";
-let str2 = 'Hello World';
-let str3 = `
-    hello 
-    world
-`;// template literal (backtick above tab key)
+function getAssignmentMark(student,course,num){
+    for(let record of student.transcript){
+        if(record.course === course){
+            if(num >= 0 && num < record.grades.length){
+                return record.grades[num];
+            }
+        }
+    }
+    return -1;
+}
 
-console.log(str1, str2, str3);
+function averageAssessment(students, courseName, assignment){
+    for(let _student of students){
+        for(let record of _student.transcript){
+            if(record.course === courseName){
+                if(assignment >= 0 && assignment < record.grades.length){
+                let sum = 0;
+                for(let grade of record.grades){
+                    sum += record.grades[assignment];
+                }
+                return sum / record.grades.length;
+                }
+            }
+        }
+    }
+    return -1;
+}
+
+console.log(getAverageGrade(bob,'INFO 1603'))
+console.log(getAssignmentMark(sally,'INFO 1601',2))
+console.log(averageAssessment(students,'INFO 1600',2))
